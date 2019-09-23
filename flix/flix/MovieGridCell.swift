f3b9602a9a82c933a3786fb1570f5221124646bf
Aaron Parks <aaparks@csumb.edu>
2019-09-23 13:11:55 -0700

    finished part 2 of the assignment

---
+++ flix/flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Aaron Parks on 9/16/19.
+//  Copyright © 2019 Aaron Parks. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
